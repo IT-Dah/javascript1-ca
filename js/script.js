481364eda134809180bd0eaa62fbda0d0570d2ed
IdaDah <idadah00216@stud.noroff.no>
2024-02-28 12:08:33 +0100

    fetched the API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const API_BASE_URL = "https://v2.api.noroff.dev";
 
-export const API_RAINYDAYS_URL = '${API_BASE_URL}/rainy-days';
+export const API_RAINYDAYS_URL = `${API_BASE_URL}/rainy-days`;
+
+import {doFetch} from './doFetch.js';
 
 async function main() {
     const responseData = await doFetch(API_RAINYDAYS_URL);
