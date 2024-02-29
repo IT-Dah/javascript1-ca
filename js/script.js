b27890ce3d3e4806309d2e3ab5b01267cd2f8f13
IdaDah <idadah00216@stud.noroff.no>
2024-02-29 10:36:46 +0100

    creating elements

--- js/script.js
+++ js/script.js
@@ -4,9 +4,62 @@ export const API_RAINYDAYS_URL = `${API_BASE_URL}/rainy-days`;
 
 import {doFetch} from './doFetch.js';
 
+/* <div class="jacket-display" ;>
+          <img
+            src="https://i.ibb.co/t8GfZnL/tibetian-red1-min.jpg"
+            alt="tibetian-red whisper jacket"
+          />
+          <div class="product-info">
+            <h4>Whisper Jacket</h4>
+            <p>Tibetian Red</p>
+            <p>kr 699,-</p>
+            <form action="product.html">
+              <button type="submit">Add To Cart</button>
+            </form>
+          </div>
+        </div>
+        */
+
+function generateJacketHtml(jacket) {
+    // returns game HTML
+    const jacketDisplay = document.createElement('div');
+    jacketDisplay.classList.add('jacket-display');
+
+    //does the image go inside here?
+    const jacketImage = document.createElement('img');
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const heading = document.createElement('h4');
+    heading.textContent = jacket.title;
+
+    const jacketPriceContainer = document.createElement('div');
+    jacketPriceContainer.classList.add('jacketPriceContainer')
+
+    const jacketPrice = document.createElement('div');
+    jacketPrice.textContent = game.price;
+
+    const jacketDiscountedPrice = document.createElement('div');
+    jacketDiscountedPrice.textContent = jacket.discountedPrice;
+
+    jacketPriceContainer.append(jacketPrice, jacketDiscountedPrice);
+
+    return jacketDisplay;
+    
+
+function displayJackets(jackets) {
+    console.log(jackets);
+    jackets.forEach((jacket) => {
+        const jacketHtml = generateJacketHtml(jacket);
+    });
+}
+
 async function main() {
     const responseData = await doFetch(API_RAINYDAYS_URL);
-    console.log(responseData);
+    const jackets = responseData.data;
+    displayJackets(jackets)
+    
 }
 
 main();
